a6dacb4cea540e92b5083d5d8f780bdf2a54b573
Abhyuday-Dev <abhyuday7176@hotmail.com>
2023-06-27 19:29:08 +0530

    changes in design

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  // Other CORS headers like methods, headers, etc. can be added here if needed
   next();
 });
 
@@ -32,7 +31,7 @@ const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
 
-    app.listen(8000, () => console.log("Server started on port 8000"));
+    app.listen(8001, () => console.log("Server started on port 8001"));
   } catch (error) {
     console.log(error);
   }
