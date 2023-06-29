a62d7e369d8e52e086e654caa3ba2898a29c3936
Abhyuday-Dev <abhyuday7176@hotmail.com>
2023-06-29 15:08:46 +0530

    e

--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use((err, req, res, next) => {
+  if (err.name === 'CORSError') {
+    console.log('CORS Error:', err.message);
+  }
+  next(err);
+});
 app.use(express.json({ limit: "50mb" }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
